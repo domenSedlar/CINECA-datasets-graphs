05b833141e58f4bf6e5c7d19ebf9cc9383cdeef4
domenSedlar <domen.sedlar@gmail.com>
2025-09-03 18:01:03 +0200

    added logs

--- GNN/main.py
+++ GNN/main.py
@@ -12,14 +12,14 @@ def main():
     state_file='GraphCreation/StateFiles/state.parquet'
     stop_event = threading.Event()
 
-    model = MyModel(builder_output_queue, train_on=6000, repeat=10) # TODO set optional parameters
+    model = MyModel(builder_output_queue, train_on=10000, repeat=10) # TODO set optional parameters
 
     kwargs_graph_creation = {
         "reader_output_queue" : reader_output_queue,
         "builder_output_queue" : builder_output_queue,
         "state_file" : state_file,
         "stop_event" : stop_event,
-        "num_limit" : 12000,
+        "num_limit" : 20000,
         "nodes" : {2},
         "skip_None": True
     }
